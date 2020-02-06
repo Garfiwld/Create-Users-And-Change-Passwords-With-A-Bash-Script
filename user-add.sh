983ff4fa01320baef0072c110c0a460105391ea9
GarFiwld <garfiwld@icloud.com>
2020-02-06 12:29:00 +0700

    Update user-add.sh
    add pass

--- user-add.sh
+++ user-add.sh
@@ -3,17 +3,19 @@ uOld=0
 uNew=0
 for i in `more userlist.txt `
 do
-egrep "$i" /etc/passwd >/dev/null
+user=$(echo $i | cut -d ":" -f 1)
+pass=$(echo $i | cut -d ":" -f 2)
+egrep "$user" /etc/passwd >/dev/null
 if [ $? -eq 0 ];
 then
 	uOld=`expr $uOld + 1`
-	echo "$i exists!"
+	echo "$user exists!"
 else
 	uNew=`expr $uNew + 1`
-	#adduser --disabled-password --gecos "First Last,RoomNumber,WorkPhone,HomePhone" $i 1>/dev/null 2>&1
-	adduser --disabled-password --gecos "$i" $i 1>/dev/null 2>&1
-	echo "$i:Password" | chpasswd 1>/dev/null 2>&1
-	echo "Add User $i"
+	#--gecos "First Last,RoomNumber,WorkPhone,HomePhone"
+	adduser --disabled-password --gecos "$user" $user 1>/dev/null 2>&1
+	echo "$user:$pass" | chpasswd 1>/dev/null 2>&1
+	echo "Add User $user"
 fi
 done
 echo "AllUser:$(cat userlist.txt | wc -l) | OldUser:$uOld | NewUser:$uNew"
